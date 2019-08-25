7569602df8c2892dfa1edbf87d4c15cfa469dca7
hassaanalansary <hassaanalansary@yahoo.com>
2019-08-26 01:00:20 +0200

    Add login link and repo link

--- job_app/views/view1.py
+++ job_app/views/view1.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 def view1(request):
     if not request.user.is_authenticated:
-        return HttpResponse("You are not logged in\n please log in using username='user1' or 'user2' and password='testuser'")
+        return HttpResponse(
+            "<h3>You are not logged in\n please log in using username='user1' or 'user2' and password='testuser'</h3>"
+            "<a href='/admin'>login here</a>")
     if not request.user.groups.filter(name='viewer1').exists():
         return HttpResponse("You are not authorized to view this page")
     return HttpResponse("This is View 1")
